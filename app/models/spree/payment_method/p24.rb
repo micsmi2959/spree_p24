c5a9f0d1184e26d1ed8dfa2400f606814d35c397
micsmi2959 <mic.smiechowicz@gmail.com>
2018-04-18 13:14:49 +0200

    ext

--- app/models/spree/payment_method/p24.rb
+++ app/models/spree/payment_method/p24.rb
@@ -4,11 +4,6 @@ module Spree
     preference :p24_merchant_id, :string
     preference :p24_pos_id, :string
     preference :crc_key, :string
-
-
-    preference :p24_id_sprzedawcy, :string
-    preference :p24_language, :string, :default => "pl"
-    preference :crc_key, :string
     preference :test_mode, :boolean, :default => false
 
   end
